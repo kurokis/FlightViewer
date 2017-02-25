98f9301a98de1fe9f467c4bef431219ce324caf0
Shota Kuroki <kurokis@users.noreply.github.com>
2017-02-26 01:10:37 +0900

    added 3D map using Vizicities

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
 cd ../
-call asar pack ./ui ./ui.asar
-call electron-packager ./ui ui --platform=win32 --arch=x64 --version=1.4.13 --overwrite
+call asar pack ./FlightViewer ./FlightViewer.asar
+call electron-packager ./FlightViewer FlightViewer --platform=win32 --arch=x64 --version=1.4.13 --overwrite
